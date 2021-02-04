eab7ce7221deb22bd0b70a0a439f1fbb5706487d
christian <christian@enclave.vn>
2021-02-04 13:22:43 +0700

    test get hi

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -18,5 +18,9 @@ describe('AppController', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
+
+    it('should return "Hi"', () => {
+      return expect(appController.getHi()).toBe("Hi");
+    })
   });
 });
